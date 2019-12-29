454350d0bdd6b6a16be0904b3c90018ab063f431
maratib <maratib@gmail.com>
2019-12-30 00:47:30 +0500

    multiple updates

--- front-page.php
+++ front-page.php
@@ -5,24 +5,22 @@
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
  *
- *  @package WordPress
- *
- * @subpackage Twenty_Thirteen
- *
- * @since Twenty Thirteen 1.0
+ * @package my-wp-boostrap-starter-theme
  */
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Front Page 123</h1>
 </body>
+
 </html>
